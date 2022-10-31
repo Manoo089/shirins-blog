6c6729468060f3990cef9bde62aef43522c6c9d1
Manuel Hohenadl <hm@campudus.com>
2022-10-31 14:07:38 +0100

    configure project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+const globImporter = require("node-sass-glob-importer");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  sassOptions: {
+    includePaths: [path.join(__dirname, "components", "styles")],
+    importer: globImporter(),
+  },
+};
+
+module.exports = nextConfig;
